48661bbcc5220a72217ca5e62079cae623d4d012
Omar-macbook <omar987@hotmail.co.uk>
2022-04-24 13:55:35 +0100

    fix order error causing navbar mobile menu to hide close icon

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-
 import { HiMenuAlt4, HiX } from 'react-icons/hi';
 import { motion } from 'framer-motion';
 
 import { images } from '../../constants';
 import './Navbar.scss';
+
 const Navbar = () => {
 	const [toggle, setToggle] = useState(false);
 
@@ -21,16 +21,17 @@ const Navbar = () => {
 					</li>
 				))}
 			</ul>
+
 			<div className="app__navbar-menu">
 				<HiMenuAlt4 onClick={() => setToggle(true)} />
 
 				{toggle && (
 					<motion.div
 						whileInView={{ x: [300, 0] }}
-						transition={{ duration: 0.85, ease: 'ease-in-out' }}
+						transition={{ duration: 0.85, ease: 'easeOut' }}
 					>
+						<HiX onClick={() => setToggle(false)} />
 						<ul>
-							<HiX onClick={() => setToggle(false)} />
 							{['home', 'about', 'work', 'skills', 'contact'].map((item) => (
 								<li key={item}>
 									<a href={`#${item}`} onClick={() => setToggle(false)}>

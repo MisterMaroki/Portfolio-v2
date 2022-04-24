40895c0326e14bfe1878cbd0e7a3d96977b94643
Omar-macbook <omar987@hotmail.co.uk>
2022-04-24 12:59:48 +0100

    navbar styles initialised, looking good for most part. no state yet or routes

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 
-import images from '../images';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
+
+import { images } from '../../constants';
 import './Navbar.scss';
 const Navbar = () => {
 	return (
-		<nav>
-			<div>
+		<nav className="app__navbar">
+			<div className="app__navbar-logo">
 				<img src={images.logo} alt="logo" />
+			</div>
+			<ul className="app__navbar-links">
+				{['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+					<li className="app__flex p-text" key={`link-${item}`}>
+						<div />
+						<a href={`#${item}`}>{item}</a>
+					</li>
+				))}
+			</ul>
+			<div className="app__navbar-menu">
+				<HiMenuAlt4 onClick={()}/>
 			</div>
 		</nav>
 	);

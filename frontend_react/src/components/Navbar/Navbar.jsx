6f6cbf6252e5d28706cc8b8e81333905cad55e83
Omar-macbook <omar987@hotmail.co.uk>
2022-04-24 13:48:38 +0100

    navbar styles -- added responsive hanburger menu 80% done

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { HiMenuAlt4, HiX } from 'react-icons/hi';
 import { motion } from 'framer-motion';
@@ -6,6 +6,8 @@ import { motion } from 'framer-motion';
 import { images } from '../../constants';
 import './Navbar.scss';
 const Navbar = () => {
+	const [toggle, setToggle] = useState(false);
+
 	return (
 		<nav className="app__navbar">
 			<div className="app__navbar-logo">
@@ -20,7 +22,25 @@ const Navbar = () => {
 				))}
 			</ul>
 			<div className="app__navbar-menu">
-				<HiMenuAlt4 onClick={()}/>
+				<HiMenuAlt4 onClick={() => setToggle(true)} />
+
+				{toggle && (
+					<motion.div
+						whileInView={{ x: [300, 0] }}
+						transition={{ duration: 0.85, ease: 'ease-in-out' }}
+					>
+						<ul>
+							<HiX onClick={() => setToggle(false)} />
+							{['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+								<li key={item}>
+									<a href={`#${item}`} onClick={() => setToggle(false)}>
+										{item}
+									</a>
+								</li>
+							))}
+						</ul>
+					</motion.div>
+				)}
 			</div>
 		</nav>
 	);

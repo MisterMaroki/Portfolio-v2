b35168e7008906abc22c4f9e91c26c6d2c91bd0c
Omar-macbook <omar987@hotmail.co.uk>
2022-05-17 20:16:31 +0100

    iconButton colors

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -11,7 +11,7 @@ const SocialMedia = () => {
 				target="_blank"
 				rel="noopener noreferrer"
 			>
-				<IconButton variant="outlined">
+				<IconButton>
 					<GitHubIcon />
 				</IconButton>
 			</a>

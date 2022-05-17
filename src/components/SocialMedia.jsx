0d5319fe621de2a45014299d2fd1a9ea49b2a78b
Omar-macbook <omar987@hotmail.co.uk>
2022-05-17 19:59:41 +0100

    update hoc with links and optimise imgs

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,18 +1,38 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import { IconButton } from '@mui/material';
+import { EmailOutlined } from '@mui/icons-material';
 const SocialMedia = () => {
 	return (
 		<div className="app__social">
-			<div>
-				<BsTwitter />
-			</div>
-			<div>
-				<FaFacebookF />
-			</div>
-			<div>
-				<BsInstagram />
-			</div>
+			<a
+				href="https://github.com/MisterMaroki"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<IconButton variant="outlined">
+					<GitHubIcon />
+				</IconButton>
+			</a>
+			<a
+				href="https://www.linkedin.com/in/omarmaroki/"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<IconButton>
+					<LinkedInIcon />
+				</IconButton>
+			</a>
+			<a
+				href="mailto:omar.maroki@outlook.com"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<IconButton>
+					<EmailOutlined />
+				</IconButton>
+			</a>
 		</div>
 	);
 };

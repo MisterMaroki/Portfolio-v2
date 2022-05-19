0ba9ff38ef1d054aed6de42401056f38adb53eab
Omar-macbook <omar987@hotmail.co.uk>
2022-05-19 19:42:23 +0100

    timing fixed

--- src/wrapper/useOnScreen.js
+++ src/wrapper/useOnScreen.js
@@ -5,7 +5,7 @@ export default function useOnScreen(ref) {
 
 	const observer = new IntersectionObserver(
 		([entry]) => setIntersecting(entry.isIntersecting),
-		{ threshold: 0.4 }
+		{ threshold: 0.5 }
 	);
 
 	useEffect(() => {

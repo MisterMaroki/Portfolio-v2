ebcb2135b03cf8f9b614cc1d0092813e90d020d9
Omar Maroki <omar.maroki@tappit.com>
2023-01-15 11:33:10 +0000

    Add redirect to content studio at /sanity

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { About, Footer, Header, Skills, Testimonial, Work } from './container';
 import { Navbar } from './components';
 import './App.scss';
 const App = () => {
+	if (window.location.href.endsWith('sanity'))
+		window.location.href =
+			'https://portfolio-backend-mistermaroki.vercel.app/desk';
+
 	return (
 		<div className="app" id="home">
 			<Navbar />
